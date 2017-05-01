1f6834c39db7c21b7c9cb5643c9b62e594712f6b
Alison <lee.alison@nyu.edu>
2017-05-01 14:25:50 -0400

    Interactivity

--- js/drawing-3.js
+++ js/drawing-3.js
@@ -6,6 +6,8 @@ var link;
 var a = 0.0;
 var s = 0.0;
 
+var e1, e2, e3, e4, e5, e6, e7, e8;
+
 function setup() {
   createCanvas(1000, 300);
   frameRate(30);
@@ -27,8 +29,6 @@ function draw() {
   stroke(30);
   rect(10, 10, 980, 280);
 
-  
-
   if ((mouseX >= 400 && mouseX <= 585) && (mouseY >= 100 && mouseY <= 150)) {
     cursor(HAND);
     link = true;
@@ -40,64 +40,44 @@ function draw() {
   // Bubbles - L to R
   noStroke();
   fill(255);
-  ellipseMode(CENTER);
 
-  //scale(s); 
-  if ((mouseX >= x+200 && mouseX <= x+250) && (mouseY >= y+30 && mouseY <= y+80)) {
-    scale(2);
-  }
-  else {
-    scale(1);
-  }
+  ellipseMode(CENTER);
+  var r1 = map(mouseX, 0, width, 0, height-40);
+  var r2 = height-40-r1;
+  
+  fill(r1);
+  ellipse(width/2 + r1/2, height/2, r1, r1);
+  
+  fill(r2);
+  ellipse(width/2 - r2/2, height/2, r2, r2);
+
   ellipse(x+200, y+30, 50, 50);
 
-
-  //scale(s); 
   ellipse(x+250, y+10, 20, 20);
 
-  //scale(s); 
   ellipse(x+400, y, 50, 50);
 
-  //scale(s); 
   ellipse(x+500, y+50, 85, 85);
 
-  //scale(s); 
   ellipse(x+500, y-100, 30, 30);
 
-  //scale(s); 
   ellipse(x+560, y-80, 70, 70);
 
-  //scale(s); 
   ellipse(x+700, y+30, 50, 50);
 
-  //scale(s); 
   ellipse(x+770, y-80, 100, 100);
 
   noStroke();
   fill(0);
   text("spring", 400, 150); 
   textFont("Grouch");
-  textSize(50);
-
-   //  ellipseMode(CENTER);
-   //  fill(255,182,193);
-   //  ellipse(x, y, 200, 200);
-
-   //  x += speed;
-   //  y += speed - 2;
-
-   //  if ((x > width) || (x < 0)) {
-   //  	speed = speed * -1;
-  	// }
-  	// if ((y > height) || (y <0)) {
-  	// 	speed = speed * -1;
-  	// }
-
-  
+  textSize(50);  
 }
 
 function mousePressed() {
   if (link == true) {
     window.open( "spring.html", "_blank");
   }
+  
+  scale(0.5);
 }
