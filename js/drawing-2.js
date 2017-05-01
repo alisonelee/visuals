1a8491ae157b578f74bbfb50cbf09be578fb39c1
Alison <lee.alison@nyu.edu>
2017-04-30 22:09:14 -0400

    Added stylesheets

--- js/drawing-2.js
+++ js/drawing-2.js
@@ -3,9 +3,11 @@ var pxd;
 var pxWidth;
 var pxHeight;
 var link;
+var img;
 
 function setup() {
   createCanvas(1000, 300);
+  img = loadImage("../media/ekg.gif"); // Photo credit goes to http://www.12leadecg.com/full/ecgindex.aspx?id=13
 }
 
 function draw() {
@@ -16,6 +18,7 @@ function draw() {
   stroke(30);
   rect(10, 10, 980, 280);
 
+  // Checks whether "link" is clicked
   if ((mouseX >= 400 && mouseX <= 600) && (mouseY >= 100 && mouseY <= 150)) {
     cursor(HAND);
     link = true;
@@ -29,10 +32,13 @@ function draw() {
   text("winter", 400, 150); 
   textFont("Grouch");
   textSize(50);
+
+  image(img, 0, 0);
+  image(img, 710, 0);
 }
 
 function mousePressed() {
   if (link == true) {
-    window.open( "drawing-2.html", "_blank");
+    window.open( "winter.html", "_blank");
   }
 }
--- js/drawing-3.js
+++ js/drawing-3.js
@@ -3,14 +3,21 @@ var x = 150;
 var y = 150;
 var speed = 10;
 var link;
+var a = 0.0;
+var s = 0.0;
 
 function setup() {
   createCanvas(1000, 300);
+  frameRate(30);
   xpos = width/2;
   ypos = height/2;
 }
 
 function draw() {
+
+  a = a + 0.05;
+  s = cos(a) * 2;
+
 	stroke(255);
 
   rectMode(CORNER);
@@ -20,6 +27,8 @@ function draw() {
   stroke(30);
   rect(10, 10, 980, 280);
 
+  
+
   if ((mouseX >= 400 && mouseX <= 585) && (mouseY >= 100 && mouseY <= 150)) {
     cursor(HAND);
     link = true;
@@ -27,6 +36,49 @@ function draw() {
     cursor(ARROW);
     link = false;
   }
+
+  // Bubbles - L to R
+  noStroke();
+  fill(255);
+  ellipseMode(CENTER);
+
+  //scale(s); 
+  if ((mouseX >= x+200 && mouseX <= x+250) && (mouseY >= y+30 && mouseY <= y+80)) {
+    scale(2);
+  }
+  else {
+    scale(1);
+  }
+  ellipse(x+200, y+30, 50, 50);
+
+
+  //scale(s); 
+  ellipse(x+250, y+10, 20, 20);
+
+  //scale(s); 
+  ellipse(x+400, y, 50, 50);
+
+  //scale(s); 
+  ellipse(x+500, y+50, 85, 85);
+
+  //scale(s); 
+  ellipse(x+500, y-100, 30, 30);
+
+  //scale(s); 
+  ellipse(x+560, y-80, 70, 70);
+
+  //scale(s); 
+  ellipse(x+700, y+30, 50, 50);
+
+  //scale(s); 
+  ellipse(x+770, y-80, 100, 100);
+
+  noStroke();
+  fill(0);
+  text("spring", 400, 150); 
+  textFont("Grouch");
+  textSize(50);
+
    //  ellipseMode(CENTER);
    //  fill(255,182,193);
    //  ellipse(x, y, 200, 200);
@@ -41,15 +93,11 @@ function draw() {
   	// 	speed = speed * -1;
   	// }
 
-  noStroke();
-  fill(0);
-  text("spring", 400, 150); 
-  textFont("Grouch");
-  textSize(50);
+  
 }
 
 function mousePressed() {
   if (link == true) {
-    window.open( "drawing-3.html", "_blank");
+    window.open( "spring.html", "_blank");
   }
 }
